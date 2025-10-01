a9d8bf8528a5912693d7cc9aa4e0381f334e71e4
TemplS <uselesssocks@gmail.com>
2025-10-01 23:18:47 +0500

    lesson4

--- 04_lesson/test_string_utils.py
+++ 04_lesson/test_string_utils.py
@@ -44,6 +44,27 @@ def test_contains_positive(word, symbol):
     ("SkyPro", "z"),
     ("SkyPro", "a")
 ])
+# def test_contains_negative(word, symbol):              У меня получилось 2 варианта решения, т.к. здесь выдает ошибку
+#     with pytest.raises(AssertionError):                какой из них более корректный - сказать что ошибка должна быть
+#         assert util.contains(word, symbol)             или поставить маркер ошибки?
+
 @pytest.mark.xfail
 def test_contains_negative(word, symbol):
     assert util.contains(word, symbol)
+
+
+@pytest.mark.parametrize("word, letter", [
+    ("SkyPro", "S"),
+    ("SkyPro", "y"),
+    ("Train", "n")
+])
+def test_delete_symbol_positive(word, letter):
+    assert util.delete_symbol(word, letter)
+
+@pytest.mark.parametrize("word, letter", [
+    ("Socks", "u"),
+    ("Moscow", "a"),
+    ("Cow", "n")
+])
+def test_delete_symbol_negative(word, letter):
+    assert util.delete_symbol(word, letter)
